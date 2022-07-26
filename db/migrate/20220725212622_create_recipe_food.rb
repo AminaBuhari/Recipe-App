5453e8f6bc4e3f75d3495b7ed0d8b43a77a4f032
Have Samuel <samiehave9@gmail.com>
2022-07-26 17:27:45 +0300

    Added the different metrics

--- db/migrate/20220725212622_create_recipe_food.rb
+++ db/migrate/20220725212622_create_recipe_food.rb
@@ -1,7 +1,7 @@
 class CreateRecipeFood < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
-      t.string :quantity
+      t.int :quantity
 
       t.timestamps
     end
