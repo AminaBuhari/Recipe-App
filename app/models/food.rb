2cfdf9c686d7776427f7f4a21b32a02e183f2b57
Have Samuel <samiehave9@gmail.com>
2022-07-26 19:04:10 +0300

    Created the model food.rb and replaced the .githun folder

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,9 @@
 class Food < ActiveRecord::Base
-  has_many :recipe_foods
+  has_many :recipe_foods dependent: :destroy
+  belong_to :user
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :measurement_unit, presence: true, length: { maximum: 3 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :quantity, presnce: true, numericality: { greater_than_or_equal_to: 0 }
 end
