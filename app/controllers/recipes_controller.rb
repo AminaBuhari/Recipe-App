aa63857b80e70be92e99d2a7333f8e2b3b9e61a3
Have Samuel <samiehave9@gmail.com>
2022-07-28 23:44:08 +0300

    Made some adjustments to the public and recipes controllers and then to the partial _nav and the recipe show_html files

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,6 +7,10 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
+  def public
+    @recipes = Recipe.where(public: true)
+  end
+  
   # GET /recipes/1 or /recipes/1.json
   def show; end
 
