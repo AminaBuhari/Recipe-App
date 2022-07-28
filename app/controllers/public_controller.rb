135f7dd855dcbfdb086384280290e399763f2a2f
Have Samuel <samiehave9@gmail.com>
2022-07-28 22:47:08 +0300

    Created the public_controller.rb, deleted the content.html.rb

---
+++ app/controllers/public_controller.rb
@@ -0,0 +1,10 @@
+class PublicsController < ApplicationController
+
+  def index
+    @user = current_user
+    @recipes = Recipe.where(user_id: current_user)
+  end
+
+  def all
+    @recipes = Recipe.where(public: true)
+  end
