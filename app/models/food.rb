9b98135df4fc82c237a8b515a221ca38a3e33774
Have Samuel <samiehave9@gmail.com>
2022-07-29 14:50:02 +0300

    Addedthe food model and made some error changes in the new.html.erb and public.html.erb files

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,12 +1,12 @@
 class Food < ApplicationRecord
   belongs_to :user
-  has_many recipe_foods, dependent: :destroy
+  has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods
 
   validates :name, uniqueness: true
-  validates :quatity, :price, numericality: { greTther_than: 0, only_float: true}
+  # validates :quantity, :price, numericality: { greater_than: 0, only_float: true}
 
-  def total_price
-    quanty * price
-  end
+  # def total_price
+  #   quanty * price
+  # end
 end
